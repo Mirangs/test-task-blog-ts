1c739aafb6746eeb77d69cc844b5aa4dc1ce003f
Max Berezanskyi <berezanskij98@gmail.com>
2020-04-06 13:10:06 +0300

    Updated styles

--- components/PostsList.tsx
+++ components/PostsList.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Posts } from '../types/post'
 import PostItem from './PostItem'
 import { Row, Col } from 'antd'
+import styled from 'styled-components'
 
 export interface PostsListProps {
   posts: Posts;
@@ -9,14 +10,18 @@ export interface PostsListProps {
 
 const PostsList: React.FC<PostsListProps> = ({ posts }) => {
   return (
-    <Row gutter={[16, 16]} justify="center">
+    <PostsRow gutter={[16, 16]} justify="center">
       {posts.map(({ title, body, id }) => (
         <Col key={id} xl={6} lg={12} md={18} sm={20} xs={24}>
           <PostItem title={title} body={body} id={id} />
         </Col>
       ))}
-    </Row>
+    </PostsRow>
   )
 }
+
+const PostsRow = styled(Row)`
+  width: 100%;
+`
 
 export default PostsList
